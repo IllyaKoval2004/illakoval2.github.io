2913dedae6d6454754fb9fc43cbee2552466310a
IllyaKoval2004 <103998747+IllyaKoval2004@users.noreply.github.com>
2022-09-27 15:35:35 +0300

    Add files via upload

---
+++ form-app.js
@@ -0,0 +1,26 @@
+function checkForm(el) {
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+
+    var error = "";
+
+    if(name == "" || phone == "" || email == "")
+        error = "empty";
+
+    if(error != "") {
+        alert("Форму не записано");
+        return false; 
+    }
+    else {
+        if(confirm('Підтвердити надсилання форми?')){
+            console.log(name + ', ' + phone + ', ' + ', ' + email);
+            alert("Форму записано");
+            return true; 
+        }
+        else {
+            alert("Форму не записано");
+            return false;
+        }
+    }
+}
---
+++ form-hide.js
@@ -0,0 +1,23 @@
+const button = document.querySelector('#a');
+const button1 = document.querySelector('#a1');
+const button2 = document.querySelector('#a2');
+const button3 = document.querySelector('#a3');
+const form = document.querySelector('form');
+
+button.addEventListener('click', () => {
+  form.classList.add('open');
+});
+
+button1.addEventListener('click', () => {
+  form.classList.add('open');
+});
+
+button2.addEventListener('click', () => {
+  form.classList.add('open');
+});
+
+button3.addEventListener('click', () => {
+  form.classList.add('open');
+});
+
+
